37e08fbed6b7f2aae166fe83f2b7abf08f907e9b
adriandotdev <marcelonads@gmail.com>
2026-03-21 12:29:41 +0800

    add category entity

--- Models/ProductContext.cs
+++ Models/ProductContext.cs
@@ -5,4 +5,13 @@ public class ProductContext : DbContext
     public ProductContext(DbContextOptions<ProductContext> options) : base(options) {}
 
     public DbSet<Product> Products {get; set;} = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasMany(e => e.Products)
+            .WithOne(e => e.Category)
+            .HasForeignKey(e => e.CategoryId)
+            .IsRequired();
+    }   
 }
